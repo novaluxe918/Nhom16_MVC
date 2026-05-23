271750eee0c1835ca8849888f0ce8a6007d711f5
PhanMaiHoaiNhi <hoainhi8999@gmail.com>
2026-05-24 00:10:46 +0700

    cap nhat models san bong

--- Nhom16_MVC/Program.cs
+++ Nhom16_MVC/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
-using Nhom16_MVC.Data;
+using Nhom16_MVC.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,9 +10,7 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(
-        builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<DatabaseService>();
 
 
 
---
+++ Nhom16_MVC/Services/StadiumService.cs
@@ -0,0 +1,14 @@
+﻿namespace Nhom16_MVC.Services
+{
+    public class StadiumService
+    {
+        private readonly DatabaseService _db;
+        
+        public StadiumService(DatabaseService db)
+        {
+            _db = db;
+        }
+
+       
+    }
+}
